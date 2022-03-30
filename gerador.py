c0f2be67a8171be98c140ac85a2c6d18cb141748
Ieremies Romero <ieremies@gmail.com>
2022-03-29 21:13:20 -0300

    Working code

--- gerador.py
+++ gerador.py
@@ -7,11 +7,11 @@ class Gerador ( object ):
         L = rand ( 5, 10 )
         M = rand ( 5, 10 )
         P = rand ( 5, 10 )
-        D =   [ [ rand(10,20 ) for i in range(P) ] for i in range(J) ]
-        r = [ [ [ rand( 1, 5 ) for i in range(L) ] for i in range(P) ] for i in range(M) ]
-        R =   [ [ rand(80,100) for i in range(F) ] for i in range(M) ]
-        C =   [ [ rand(80,100) for i in range(F) ] for i in range(L) ]
-        C_p = [ [ [ rand(10,100) for i in range(F) ] for i in range(L) ] for i in range(P) ]
-        C_t = [ [ [ rand(10,20 ) for i in range(J) ] for i in range(F) ] for i in range(P) ]
+        D =   [ [ rand(10,20 ) for j in range(J) ] for p in range(P) ]
+        r = [ [ [ rand( 1, 5 ) for l in range(L) ] for m in range(P) ] for p in range(M) ]
+        R =   [ [ rand(80,100) for f in range(F) ] for m in range(M) ]
+        C =   [ [ rand(80,100) for l in range(L) ] for f in range(F) ]
+        C_p = [ [ [ rand(10,100) for l in range(L) ] for f in range(F) ] for p in range(P) ]
+        C_t = [ [ [ rand(10,20 ) for j in range(J) ] for f in range(F) ] for p in range(P) ]
 
         return (J, F, L, M, P, D, r, R, C, C_p, C_t)
--- solver.py
+++ solver.py
@@ -1,37 +1,49 @@
 import gurobipy as gp
 from gurobipy import GRB
+from gerador import Gerador
 
-model = gp.Model('Atividade 1')
+class Solver:
+    def solve(self, J, F, L, M, P, D, r, R, C, C_p, C_t):
+        model = gp.Model("Atividade 1")
+        # Variables declaration
+        x = [ [ [ model.addVar(lb=0.0, ub=GRB.INFINITY, obj=C_p[p][f][l] , vtype=GRB.INTEGER, name=f"x[{p}][{f}][{l}])") for l in range(L) ] for f in range(F) ] for p in range(P) ]
+        y = [ [ [ model.addVar(lb=0.0, ub=GRB.INFINITY, obj=C_t[p][f][j] , vtype=GRB.INTEGER, name=f"y[{p}][{f}][{j}])") for j in range(J) ] for f in range(F) ] for p in range(P) ]
 
-# Variables declaration
-x = [ [ [ model.addVar(0.0, GBR.INFINITY, 0.0, GRB.INT, f"x[{i}][{j}][{k}])")
-            for i in range(L) ]
-        for j in range(F) ]
-        for k in range(P) ]
-y = [ [ [ model.addVar(0.0, GBR.INFINITY, 0.0, GRB.INT, f"x[{i}][{j}][{k}])")
-            for i in range(J) ]
-        for j in range(F) ]
-        for k in range(P) ]
-model.update()
+        model.update()
+        model.ModelSense = GRB.MAXIMIZE
 
-# Objective function
-expr = GRBLinExpr()
-for p in range(P):
-    for f in range(F):
-        # x_{p,f,l} terms
-        for l in range(L):
-            expr.addTerm(C_p[p][f][l], x[p][f][l])
-        # y_{p,f,j} terms
-        for j in range(J):
-            expr.addTerm(C_t[p][f][j], x[p][f][j])
-model.setObjective(expr, GRB.MAXIMIZE)
+        # RESTRICTIONS
+        # Demmand constrains
+        model.addConstrs(
+            ( sum([y[p][f][j] for f in range(F)]) <= D[p][j] for j in range(J) for p in range(P)),
+            name="demmand"
+        )
+        # Produced equals transported constrains
+        model.addConstrs(
+            (sum(x[p][f]) - sum(y[p][f]) == 0 for f in range(F) for p in range(P)),
+            name="prod equals transp"
+        )
+        # Material availability constrains
+        model.addConstrs(
+            (sum([x[p][f][l] for p in range(P) for l in range(L)]) <= R[m][f] for f in range(F) for m in range(M)),
+            name="material"
+        )
+        # Capacity constrains
+        model.addConstrs(
+            (sum([x[p][f][l] for p in range(P)]) <= C[f][l] for f in range(F) for l in range(L)),
+            name="capacity"
+        )
 
-# Restrictions
-## TODO Demmand constrains
-## TODO Produced equals transported constrains
-## TODO Material availability constrains
-## TODO Capacity constrains
+        model.optimize()
 
-model.optimize()
+        print("Final =", model.ObjVal)
+        model.write("Atividade 1.json")
+        model.dispose()
 
-# TODO Print result
+if __name__ == "__main__":
+    g = Gerador()
+    s = Solver()
+    for i in range(11):
+        J, F, L, M, P, D, r, R, C, C_p, C_t = g.gen(i*100)
+        print("Instância = ",J, F, L, M, P)
+        s.solve(J, F, L, M, P, D, r, R, C, C_p, C_t)
